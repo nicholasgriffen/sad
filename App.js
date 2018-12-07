a0a66527fb93f208644dc7acb57009b03fadc31f
Nicholas <nicholas.s.griffen@gmail.com>
2018-12-07 14:33:15 -0700

    sunrise/set logic

--- App.js
+++ App.js
@@ -2,6 +2,48 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+      sunrise: 0,
+      sunset: 0
+    }
+    this.API = `https://api.sunrise-sunset.org/json?`
+
+  }
+  
+  componentDidMount() {
+    this.getLocation()
+  }
+
+  async fetchTimes(latitude, longitude) {
+    const req = await fetch(`${this.API}lat=${latitude}&lng=${longitude}&formatted=0&callback=?`).catch(e => Alert.alert(e.message))
+    const text = await req.text()
+    const match = text.match(/\?\((.*)\);/)
+    const results = JSON.parse(match[1])["results"]
+    //
+    this.setState({
+        sunrise: new Date(`${results["sunrise"]} UTC`),
+        sunset: results["sunset"]
+    })
+}
+
+  getLocation() {
+    navigator.geolocation.getCurrentPosition((loc) => {
+      this.fetchTimes(loc.coords.latitude, loc.coords.longitude)
+    }, (e) => { 
+      if (e.code === 'E_LOCATION_SERVICES_DISABLED') {
+        Alert.alert('Please enable location to use this feature')
+      } else {
+        Alert.alert(e.code)        
+      }
+      this.setState({
+      ...this.state, 
+      errors: e
+    })})
+  }
   render() {
     return (
       <View style={styles.container}>
